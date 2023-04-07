363769e641156374343a0c99ee60bf8969dbb631
Katherine Vilinski-Mazur <Katherine.Vilinski-Mazur@skoltech.ru>
2023-04-07 22:41:37 +0300

    ok

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // Итерационный подсчет факториала
 function factIterat(num){
-
+    let res = 1;
+    while(num){
+        res *= num--;
+    }
+    return res;
+    
 }
 
 // Рекурсивный расчет факториала
 function factRec(num){
- 
+    return (num != 1)?num * factRec(num - 1):1;
 }
+console.log(factIterat(5));
